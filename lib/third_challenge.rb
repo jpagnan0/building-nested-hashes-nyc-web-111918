d50382a89c7c62b57abdc5dc9e8d419dbbd7868b
Adam Saland <a.jpagnano@gmail.com>
2018-11-09 16:42:45 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,16 +1,18 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        {name: "Lord Montague", age: "53"},
-      },
-      :matriarch => {
-        {name: "Lady Montague", age: "54"},
-      },
-      :hero => {
-        
-      },
-      :hero_friends => []
+     :patriarch => {
+      
+       
+     },
+     :matriarch => {
+       
+     },
+     :hero => {
+      
+     },
+    :hero_friends => []
+     
    }, 
    :capulet => {
       :patriarch => {
